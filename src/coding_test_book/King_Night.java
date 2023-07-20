dcee41cdfb5e90bc6cdab1da814e532d6175b00a
seunghye <laurel8310@naver.com>
2023-07-20 21:51:46 +0900

    23.07.20 이코테 시각, 왕실의 나이트

---
+++ src/coding_test_book/King_Night.java
@@ -0,0 +1,28 @@
+package coding_test_book;
+
+import java.util.Scanner;
+
+public class King_Night {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String place = sc.nextLine();
+        int row = place.charAt(1) - '0';
+        int column = place.charAt(0) - 'a' + 1;
+
+        int[] dx = {-2, -2, 1, 2, 2, 1, -1, -2};
+        int[] dy = {-1, -2, -2, -1, 1, 2, 2, 1};
+
+        int result = 0;
+
+        for(int i = 0; i < 8; i++) {
+            int nextRow = row + dx[i];
+            int nextColumn = column + dy[i];
+
+            if(nextRow >= 1 && nextRow <= 8 && nextColumn >= 1 && nextColumn <= 8) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
