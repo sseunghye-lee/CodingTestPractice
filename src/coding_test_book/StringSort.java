89a7aa7fad400c45e70f948b539e2a247090f5e5
seunghye <laurel8310@naver.com>
2023-07-21 15:31:10 +0900

    23.07.21 이코테 문자열 재정렬

---
+++ src/coding_test_book/StringSort.java
@@ -0,0 +1,30 @@
+package coding_test_book;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class StringSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        ArrayList<Character> result = new ArrayList<>();
+        int value = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(Character.isLetter(str.charAt(i))) {
+                result.add(str.charAt(i));
+            } else {
+                value += str.charAt(i) - '0';
+            }
+        }
+
+        Collections.sort(result);
+
+        for(int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+        }
+
+        if(value != 0) System.out.println(value);
+    }
+}
