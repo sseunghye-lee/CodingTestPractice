74b99beb7bc31bcc9527e7937ac25d357759c8c6
seunghye <laurel8310@naver.com>
2024-03-20 18:19:56 +0900

    24.03.20 인프런_자바 코테 초급 section4 - 심사위원, 모임장소, 멀티태스킹

---
+++ src/inflearn/section4/MeetingPlace.java
@@ -0,0 +1,37 @@
+package inflearn.section4;
+
+import java.util.ArrayList;
+
+public class MeetingPlace {
+    public int solution(int[][] board){
+        int answer = 0;
+        int n = board.length;
+        ArrayList<Integer> row = new ArrayList<>();
+        ArrayList<Integer> col = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 1) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        col.sort((a, b) -> a - b);
+        int x = row.get(row.size() / 2);
+        int y = col.get(col.size() / 2);
+
+        for(int p : row) answer += Math.abs(x - p);
+        for(int p : col) answer += Math.abs(y - p);
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        MeetingPlace T = new MeetingPlace();
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
+    }
+}
